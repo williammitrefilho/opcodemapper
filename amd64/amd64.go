a1a2115fe4db085469bb2819768642e7c61473bb
williammitrefiho <willmitre@gmail.com>
2022-10-04 02:14:41 -0300

    Continuing with AMD64

--- amd64/amd64.go
+++ amd64/amd64.go
@@ -30,27 +30,35 @@ type Processor struct{
 	nFedBytes int
 	
 	rEXPrefix byte
-	mnemonicRoot string
 	finished bool
 	
 	opcode byte
 	hasOpcode bool
 	extendedOpcode bool
+	opcode_str string
 	
 	mod_r_m_Status *modRMStatus
 	sibByte *sIBByte
 	hasModRM bool
 	
-	srcOper string
-	dstOper string
+	operandSize uint8
 	
-	immediateVal int32
+	srcOper *string
+	dstOper *string
+	
+	immediateBytes uint8
+	displacementBytes uint8
+	
+	immediateVal int64
+	immediateVal_str string
+	displacementVal int64
 }
 
 func NewProcessor() *Processor{
 	
 	p := new(Processor)
 	p.mod_r_m_Status = new(modRMStatus)
+	p.sibByte = new(sIBByte)
 	return p
 }
 
@@ -62,7 +70,7 @@ func (p *Processor) feedByte(b byte) int{
 	
 		return p.setOpcode(b)
 	}
-	return 0
+	return 1
 }
 
 func (p *Processor) ReadInstruction(instructionBytes []byte){
@@ -80,27 +88,54 @@ func (p *Processor) ReadInstruction(instructionBytes []byte){
 		}
 	}
 	
-	if intBytes > 0 && index+intBytes <= len(instructionBytes){
+	boundary := index+(int)(p.displacementBytes)
+	if p.displacementBytes > 0 && boundary <= len(instructionBytes){
 		
-		var imm int32 = 0
+		var imm int64 = 0
 		o := 0
-		for i := index; i < index+intBytes; i++{
+		for i := index; i < boundary; i++{
 			
-			imm |= (int32)(instructionBytes[i]) << (8*o)
+			imm |= (int64)(instructionBytes[i]) << (8*o)
+			o++
+		}
+		p.displacementVal = imm
+	}
+	boundary += (int)(p.immediateBytes)
+	index += (int)(p.immediateBytes)
+	if p.immediateBytes > 0 && boundary <= len(instructionBytes){
+		
+		var imm int64 = 0
+		o := 0
+		for i := index; i < boundary; i++{
+			
+			imm |= (int64)(instructionBytes[i]) << (8*o)
 			o++
 		}
 		p.immediateVal = imm
 	}
+	if p.displacementVal > 0{
+		
+		p.mod_r_m_Status.r_m_str = fmt.Sprintf("%v+0x%x", p.mod_r_m_Status.r_m_str, p.displacementVal)
+	}
+	p.immediateVal_str = fmt.Sprintf("0x%x", p.immediateVal)
 }
 
 func (p *Processor) setOpcode(b byte) int{
 	
 	if p.hasOpcode{
 		
+		p.setModRM_SIB(b)
+/*		
 		highNibble := p.opcode>>4; lowNibble := p.opcode&0xF
 		if highNibble < 4{
 			
 			if lowNibble < 6 || (lowNibble > 0x7 && lowNibble < 0xE){
+				
+				return p.setModRM_SIB(b)
+			}
+		} else if highNibble == 0x8{
+			
+			if lowNibble < 0x4 || (lowNibble > 7 && lowNibble < 0xC){
 				
 				return p.setModRM_SIB(b)
 			}
@@ -111,20 +146,55 @@ func (p *Processor) setOpcode(b byte) int{
 				return p.setModRM_SIB(b)
 			}
 		}
+*/		
 	
 	} else {
 		
 		highNibble := b>>4; lowNibble := b&0xF
 		p.opcode = b
 		p.hasOpcode = true
-		if highNibble < 4{
+		if highNibble == 3{
 			
-			if lowNibble < 6 || (lowNibble > 0x7 && lowNibble < 0xE){
+			if lowNibble > 7 && lowNibble < 0xE{
 				
-				return 1
+				p.opcode_str = "CMP"
+				if lowNibble < 0xC{
+					
+					if lowNibble%2 == 0{
+						
+//						p.immediateBytes = 1
+					} else {
+						
+						if p.rEXPrefix & 0x08 != 0{
+					
+//							p.immediateBytes = 8
+						} else{
+							
+//							p.immediateBytes = 4
+						}
+					}
+					if lowNibble < 0xA{
+						
+						p.srcOper = &p.mod_r_m_Status.reg_str
+						p.dstOper = &p.mod_r_m_Status.r_m_str
+					} else {
+						
+						p.srcOper = &p.mod_r_m_Status.reg_str
+						p.dstOper = &p.mod_r_m_Status.r_m_str
+					}
+					return 1
+				}
 			}
 		} else if highNibble == 5{// POP or PUSH, no more bytes needed
 			
+			p.dstOper = &GPRNames[lowNibble]
+			if lowNibble < 8{
+				
+				p.opcode_str = "PUSH"
+			} else {
+				
+				p.opcode_str = "POP"
+			}
 			p.finished = true
 			return 0
 		} else if highNibble == 6{// POP or PUSH with more bytes needed
@@ -136,9 +206,11 @@ func (p *Processor) setOpcode(b byte) int{
 					
 					if lowNibble == 8{
 						
+						p.immediateBytes = 4
 						return 4
 					} else if lowNibble == 0xA{
 						
+						p.immediateBytes = 1
 						return 1
 					}
 				} else {//modRM byte will be needed, so just ask for one more
@@ -149,11 +221,42 @@ func (p *Processor) setOpcode(b byte) int{
 		} else if highNibble == 7{// Jump instructions that need another single byte
 			
 			p.finished = true
+			p.immediateBytes = 1
+			if lowNibble == 5{
+				
+				p.opcode_str = "JNZ"
+			}
 			return 1
 		} else if highNibble == 8{
 			
-			if lowNibble < 4{
+			if lowNibble < 4{// extended opcodes in which the instruction is specified my the ModRM byte
 				
+				return 1
+			} else if lowNibble > 0x7 && lowNibble < 0xC{// MOV instructions that require the ModRM byte
+				
+				p.opcode_str = "MOV"
+				if lowNibble%2 == 0{
+					
+					p.operandSize = 1
+				} else{
+					
+					if p.rEXPrefix & 0x08 != 0{
+						
+						p.operandSize = 8
+					} else {
+						
+						p.operandSize = 4
+					}
+				}
+				if lowNibble < 0xA{
+					
+					p.dstOper = &p.mod_r_m_Status.r_m_str
+					p.srcOper = &p.mod_r_m_Status.reg_str
+				} else{
+					
+					p.dstOper = &p.mod_r_m_Status.reg_str
+					p.srcOper = &p.mod_r_m_Status.r_m_str
+				}
 				return 1
 			}
 		} else if highNibble == 9{
@@ -163,11 +266,29 @@ func (p *Processor) setOpcode(b byte) int{
 				p.finished = true
 				return 0
 			}
+		} else if highNibble == 0xB{
+			
+			p.opcode_str = "MOV"
+			if lowNibble > 0x7{
+				
+				p.srcOper = &p.immediateVal_str
+				p.dstOper = &GPRNames[lowNibble-8]
+				if p.rEXPrefix & 0x08 != 0{
+					
+					p.immediateBytes = 8
+				} else{
+					
+					p.immediateBytes = 4
+				}
+			}
 		} else if highNibble == 0xE{
 			
 			if lowNibble == 8{
 				
+				p.opcode_str = "CALL"
 				p.finished = true
+				p.immediateBytes = 4
+				p.dstOper = &p.immediateVal_str
 				return 4
 			}
 		} else if highNibble == 0xF{
@@ -193,37 +314,70 @@ func (p *Processor) setModRM_SIB(b byte) int{
 	p.mod_r_m_Status.r_m = (b&0x7)
 	
 	p.mod_r_m_Status.reg_str = GPRNames[p.mod_r_m_Status.reg]
-	
-	if p.opcode > 0xFD{// opcode is extended by the ModRM.reg field
+	moreBytes := 0
+	if p.opcode > 0xFD || (p.opcode >= 0x80 && p.opcode <= 0x83){// opcode is extended by the ModRM.reg field
 		
-		if p.mod_r_m_Status.reg == 4{
+		p.mod_r_m_Status.reg_str = ""
+		if p.opcode > 0xFD{
 			
-			if p.mod_r_m_Status.r_m == 5{
+			if p.mod_r_m_Status.reg == 4{
 				
-				if p.mod_r_m_Status.mod == 0{
+				if p.mod_r_m_Status.r_m == 5{
 					
+					if p.mod_r_m_Status.mod == 0{
+						
+						
+					}
+				}
+				p.finished = true
+			}
+		}else {
+		
+			if p.opcode == 0x83{
+				
+				if p.mod_r_m_Status.reg == 5{
 					
+					p.opcode_str = "SUB"
+					p.dstOper = &p.mod_r_m_Status.r_m_str
+					p.srcOper = &p.immediateVal_str
+					p.immediateBytes = 1
+				} else if p.mod_r_m_Status.reg == 4{
+					
+					p.opcode_str = "AND"
+					p.dstOper = &p.mod_r_m_Status.r_m_str
+					p.srcOper = &p.immediateVal_str
+					p.immediateBytes = 1
 				}
 			}
-			p.finished = true
-			return 0
 		}
 	}
 	if p.mod_r_m_Status.mod == 3 {
 		
 		p.mod_r_m_Status.r_m_str = GPRNames[p.mod_r_m_Status.r_m]
-	} else {
+	} else if p.mod_r_m_Status.r_m_str == ""{
 		
-		if p.mod_r_m_Status.r_m != 4{
+		if p.mod_r_m_Status.r_m == 5{
+			
+			if p.mod_r_m_Status.mod == 0{
+				
+				p.mod_r_m_Status.r_m_str = ""
+				p.displacementBytes = 4
+			} else if p.mod_r_m_Status.mod == 1{
+				
+				p.mod_r_m_Status.r_m_str = "[RBP]"
+				p.displacementBytes = 1
+			}
+		} else if p.mod_r_m_Status.r_m != 4{
 			
 			p.mod_r_m_Status.r_m_str = fmt.Sprintf("[%v]", GPRNames[p.mod_r_m_Status.r_m])
 		} else {
-			// the only case when we we will need another byte, which is the SIB byte
-			return 1
+			// the only case in which we will need another byte, that being the SIB byte
+			moreBytes++
+			return moreBytes
 		}
 	}
 	p.finished = true
-	return 0
+	return moreBytes
 }
 
 func (p *Processor) setSIBByte(b byte) int{
@@ -234,36 +388,53 @@ func (p *Processor) setSIBByte(b byte) int{
 	
 	scale := 1 << p.sibByte.scale
 	var scaled_index, base string
-	moreBytes := 0
+	var moreBytes byte = 0
 	if p.sibByte.index == 4{
 		
 		scaled_index = ""
 	} else{
 		
-		scaled_index = fmt.Sprintf("%v*[%v]", scale, GPRNames[p.sibByte.index])
+		scaled_index = fmt.Sprintf("+%v*[%v]", scale, GPRNames[p.sibByte.index])
 	}
 	if p.sibByte.base == 5{
 		
 		if p.mod_r_m_Status.mod == 0{
 			
-			base = "disp32"
+			base = ""
 			moreBytes = 4
 		} else if p.mod_r_m_Status.mod == 1{
 			
-			base = "[RBP] + disp8"
+			base = "[RBP]"
 			moreBytes = 1
 		} else {
 			
-			base = "[RBP] + disp32"
+			base = "[RBP]"
+			moreBytes = 4
+		}
+	} else {
+		
+		if p.sibByte.base == 4{
+		
+			base = "[RSP]"
+		}
+		
+		if p.mod_r_m_Status.mod == 0{
+			
+			moreBytes = 0
+		} else if p.mod_r_m_Status.mod == 1{
+			
+			moreBytes = 1
+		} else{
+			
 			moreBytes = 4
 		}
 	}
-	sibOper := fmt.Sprintf("%v + %v", scaled_index, base)
+	sibOper := fmt.Sprintf("%v %v", base, scaled_index)
 	p.mod_r_m_Status.r_m_str = sibOper
 	
 	p.finished = true
-	
-	return moreBytes
+	p.displacementBytes = moreBytes
+	return (int)(moreBytes)
 }
 
 func (p *Processor) isPrefix(b byte) bool{
--- amd64/amd64_test.go
+++ amd64/amd64_test.go
@@ -11,17 +11,47 @@ func TestProcessor(t *testing.T){
 	
 	instructionBytes := []byte{
 		
-		0x78, 0x12, 0x40, 0x00,
+//		0x48, 0x89, 0x5C, 0x24, 0x20, 0x55,
+//		0xE8, 0x0B, 0x00, 0x00, 0x00,
+//		0x48, 0x89, 0x5C, 0x24, 0x20,
+//		0x55,
+//		0x48, 0x8B, 0xEC,
+//		0x48, 0x83, 0xEC, 0x20,
+//		0x48, 0x8B, 0x05, 0xB0, 0x9D, 0x10, 0x00,
+//		0x48, 0xBB, 0x32, 0xA2, 0xDF, 0x2D, 0x99, 0x2B, 0x00, 0x00,
+//		0x48, 0x3B, 0xC3
+//		0x75, 0x74,
+		0x48, 0x8B, 0x5C, 0x24,
 	}
 	
 	p.ReadInstruction(instructionBytes)
-	if p.immediateVal != 0x12{
+	if p.immediateVal != 0x0{
 		
 		t.Fatalf("imm32 = %x", p.immediateVal)
 	}
-	if p.nFedBytes != 1{
+	if p.nFedBytes != 3{
 		
-		t.Fatalf("%v fed bytes", p.nFedBytes)
+		t.Fatalf("%v fed bytes (%x)", p.nFedBytes, p.rEXPrefix)
+	}
+	if p.opcode_str != "MOV"{
+		
+		t.Fatalf("opcode_str is %v", p.opcode_str)
+	}
+	if p.displacementVal != 0x0{
+		
+		t.Fatalf("displacementVal is %x", p.displacementVal)
+	}
+	if p.dstOper != nil{
+		
+		if *p.dstOper != "RBX"{
+			t.Fatalf("dstOper = %v", *p.dstOper)
+		}
+	}
+	if p.srcOper != nil{
+		
+		if *p.srcOper != "[RBP]+0x18"{
+			t.Fatalf("srcOper = %v, sib:%v", *p.srcOper, p.sibByte)
+		}
 	}
 }
 
@@ -29,15 +59,29 @@ func TestFeedByte(t *testing.T){
 	
 	p:= NewProcessor()
 	
-	p.feedByte(0xFF)
-	r := p.feedByte(0x25)
+	pr := p.feedByte(0x48)
+	p.feedByte(0x83)
+	r := p.feedByte(0xEC)
+
 	if !p.finished{
 		
-		t.Fatalf("p is not finished. opcode: %v modrm: %v, hasModRM:%v hasOpcode:%v", p.opcode, p.mod_r_m_Status, p.hasModRM, p.hasOpcode)
+		t.Fatalf("p is not finished. opcode: %v modrm: %v, hasModRM:%v hasOpcode:%v, pr:%v r:%v", p.opcode, p.mod_r_m_Status, p.hasModRM, p.hasOpcode, pr, r)
 	}
-	if r != 0{
+	if p.immediateBytes != 1{
 		
-		t.Fatalf("r is %v", r)
+		t.Fatalf("immediateBytes is %v. ModRM.reg:%v", r, p.mod_r_m_Status.reg)
+	}
+	if p.opcode_str != "SUB"{
+		
+		t.Fatalf("opcode_str is %v", p.opcode_str)
+	}
+	if *p.dstOper != "RSP"{
+		
+		t.Fatalf("r/m is set to %v", p.mod_r_m_Status.r_m_str)
+	}
+	if *p.srcOper != ""{
+		
+		t.Fatalf("p.srcOper is set to %v", *p.srcOper)
 	}
 }
 
